8973943b7e0d22c6a119524bfe6233b426c0a359
romualdo97 <jorovipe@gmail.com>
2018-06-03 18:57:44 -0500

    Google analitycs event implemented

--- hq-lavapainter/js/coming-soon.js
+++ hq-lavapainter/js/coming-soon.js
@@ -9,4 +9,25 @@
     posterType: 'jpg'
   });
 
+  // Twitter social media
+  $('.open-twitter').on('click', function (event) {
+  	// console.log(event.currentTarget.href);
+  	handleOutboundLinkClicks('Open Twitter', event);
+  });
+
+  // Newsleter submit button
+  $('#mc-embedded-subscribe').on('click', function (event) {
+  	handleOutboundLinkClicks('Newsletter Submited', event);
+  });
+
 })(jQuery); // End of use strict
+
+
+function handleOutboundLinkClicks(action, event) {
+	console.log(action)
+  ga('send', 'event', {
+    eventCategory: 'Outbound Link',
+    eventAction: action,
+    eventLabel: event.currentTarget.href
+  });
+}
